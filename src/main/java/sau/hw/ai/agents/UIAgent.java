38a74822deeb8015d2a8948a20850571299e212e
Hamid Safdari <hamidsafdari22@gmail.com>
2017-04-13 20:46:40 +0530

    add dummy agents; update gitignore

--- src/main/java/sau/hw/ai/agents/UIAgent.java
+++ src/main/java/sau/hw/ai/agents/UIAgent.java
@@ -1,10 +1,10 @@
-package sau.hw.ai;
+package sau.hw.ai.agents;
 
 import jade.core.Agent;
 
 public class UIAgent extends Agent {
 	@Override
 	protected void setup() {
-
+		System.out.println("this is agent: " + getLocalName());
 	}
 }
---
+++ src/main/java/sau/hw/ai/ui/WeatherUI.java
@@ -0,0 +1,7 @@
+package sau.hw.ai.ui;
+
+import javax.swing.JFrame;
+
+public class WeatherUI extends JFrame {
+
+}
